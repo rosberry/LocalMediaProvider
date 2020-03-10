9dd74700e94efee87f7d808086045dba6634cce2
Alexey Korshun <lehakorshun@gmail.com>
2020-03-10 10:18:42 +0600

    AND-359 add sample module

---
+++ sample/src/main/java/com/rosberry/android/localmediaprovider/sample/SampleActivity.kt
@@ -0,0 +1,15 @@
+/*
+ *
+ *  * Copyright (c) 2019 Rosberry. All rights reserved.
+ *
+ */
+
+package com.rosberry.android.localmediaprovider.sample
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * @author Alexei Korshun on 10.03.2020.
+ */
+class SampleActivity : AppCompatActivity(R.layout.a_main) {
+}
